737a2be15f1602143d5b86d5c59eb16423877f51
gazjoy <post@garethjoyce.co.uk>
2021-05-10 14:25:41 +0100

    [Sp2 IDV] refactor ideas and add choice and email screens

--- app/views/sprint2/_routes.js
+++ app/views/sprint2/_routes.js
@@ -14,4 +14,17 @@ router.post('/add-postal-address', function (req, res) {
   }
 })
 
+
+router.post('/choice', function (req, res) {
+  var answer = req.session.data['sign-in'];
+  if (answer == 'government-gateway') {
+    res.redirect('https://www.access.service.gov.uk/login/signin/creds');
+  } else if (answer == 'govuk-verify') {
+    res.redirect('https://www.signin.service.gov.uk/start');
+  } else {
+    res.redirect(req.session.data['idvHandOffURL']);
+  }
+})
+
+
 module.exports = router
