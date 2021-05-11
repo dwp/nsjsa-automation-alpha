e03eb5aa55d358a713210eebb487eedb742441e1
gazjoy <post@garethjoyce.co.uk>
2021-05-11 13:54:08 +0100

    [Sp2 IDV] more ideas added and organised how might we problems

--- app/views/sprint2/_routes.js
+++ app/views/sprint2/_routes.js
@@ -1,30 +1,37 @@
 var express = require('express')
 var router = express.Router()
 
-router.get('/idea2', function (req, res) {
-  res.redirect('idea2/eligible');
+router.get('/1b', function (req, res) {
+  res.redirect('1b/eligible');
 })
 
-router.post('/add-postal-address', function (req, res) {
-  var answer = req.session.data['detailsAddDifferentPostalAddress'];
+router.post('/1a/choose', function (req, res) {
+  var answer = req.session.data['sign-in'];
+  if (answer == 'government-gateway') {
+    res.redirect('https://www.access.service.gov.uk/login/signin/creds');
+  } else if (answer == 'govuk-verify') {
+    res.redirect('https://www.tax.service.gov.uk/ida/login');
+  } else {
+    res.redirect(req.session.data['idvHandOffURL']);
+  }
+})
+
+router.post('/3/check-details', function (req, res) {
+  var answer = req.session.data['changed-address'];
+  if (answer == 'yes') {
+    res.redirect('new-address');
+  } else {
+    res.redirect('check-details');
+  }
+})
+
+router.post('/3-1/add-postal-address', function (req, res) {
+  var answer = req.session.data['changed-address'];
   if (answer == 'yes') {
     res.redirect('enter-postal-address');
   } else {
     res.redirect('contact-phone');
   }
 })
-
-
-router.post('/choice', function (req, res) {
-  var answer = req.session.data['sign-in'];
-  if (answer == 'government-gateway') {
-    res.redirect('https://www.access.service.gov.uk/login/signin/creds');
-  } else if (answer == 'govuk-verify') {
-    res.redirect('https://www.signin.service.gov.uk/start');
-  } else {
-    res.redirect(req.session.data['idvHandOffURL']);
-  }
-})
-
 
 module.exports = router
