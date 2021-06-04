086cc50c59051417eacf5543282e484d5a3d9903
gazjoy <post@garethjoyce.co.uk>
2021-06-04 10:24:51 +0100

    [Sprint 4] logic for idv

--- app/routes.js
+++ app/routes.js
@@ -8,9 +8,15 @@ router.use('/sprint2/', require('./views/sprint2/_routes'))
 
 // Sprint 4 routes
 router.use('/sprint4/', require('./views/sprint4/_routes'))
-router.use('/sprint4/1/1-eligibility/', require('./views/sprint4/1/1-eligibility/_routes'))
-router.use('/sprint4/2/1-eligibility/', require('./views/sprint4/2/1-eligibility/_routes'))
-router.use('/sprint4/2/dth/', require('./views/sprint4/2/dth/_routes'))
-router.use('/sprint4/2/3-details/', require('./views/sprint4/2/3-details/_routes'))
+
+router.use('/sprint4/hypothesis1/v1/1-eligibility/', require('./views/sprint4/hypothesis1/v1/1-eligibility/_routes'))
+router.use('/sprint4/hypothesis1/v1/dth/', require('./views/sprint4/hypothesis1/v1/dth/_routes'))
+router.use('/sprint4/hypothesis1/v1/3-details/', require('./views/sprint4/hypothesis1/v1/3-details/_routes'))
+router.use('/sprint4/hypothesis1/v1/4-claim-start/', require('./views/sprint4/hypothesis1/v1/4-claim-start/_routes'))
+
+router.use('/sprint4/hypothesis2/v1/1-eligibility/', require('./views/sprint4/hypothesis2/v1/1-eligibility/_routes'))
+router.use('/sprint4/hypothesis2/v1/dth/', require('./views/sprint4/hypothesis2/v1/dth/_routes'))
+router.use('/sprint4/hypothesis2/v1/3-details/', require('./views/sprint4/hypothesis2/v1/3-details/_routes'))
+router.use('/sprint4/hypothesis2/v1/4-claim-start/', require('./views/sprint4/hypothesis2/v1/4-claim-start/_routes'))
 
 module.exports = router
--- app/views/sprint4/_routes.js
+++ app/views/sprint4/_routes.js
@@ -1,8 +1,6 @@
 var express = require('express')
 var router = express.Router()
 
-// router.use('/sprint4/1/1-eligibility/', require('./views/sprint4/1/1-eligibility/_routes'))
-
 // router.get('/1b', function (req, res) {
 //   res.redirect('1b/eligible');
 // })
