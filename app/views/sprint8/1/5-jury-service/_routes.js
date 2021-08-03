cf161cec3e91783d5827b08823c69d0d0973a90b
gazjoy <post@garethjoyce.co.uk>
2021-08-03 14:54:06 +0100

    [Sprint 8] journey to prepare users to verify online

---
+++ app/views/sprint8/1/5-jury-service/_routes.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+
+const BASE_PATH = 'sprint8/1/5-jury-service';
+const ABS_BASE_PATH = `/${BASE_PATH}`;
+const NEXT_PATH = '/sprint8/1/6-current-employment';
+
+// Redirect /jury-service to first question in sequence
+router.get('/', function (req, res) {
+  res.redirect(`${ABS_BASE_PATH}/have-been`);
+});
+
+router.post('/have-been', function (req, res) {
+  // var juryConditional = req.body.juryService;
+  // if (juryConditional === 'yes') {
+  //   res.redirect('/sprint8/1/5-jury-service/period');
+  // } else {
+  //   res.redirect('/sprint8/1/6-current-employment');
+  // }
+  res.redirect(NEXT_PATH);
+});
+
+router.post('/period', function (req, res) {
+  res.redirect(NEXT_PATH);
+});
+
+module.exports = router;
---
+++ app/views/sprint8/1/8-abroad/_routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const BASE_PATH = 'sprint8/1/8-abroad';
+const ABS_BASE_PATH = `/${BASE_PATH}`;
+const NEXT_PATH = '/sprint8/1/9-pensions';
+
+router.get('/', function (req, res) {
+  res.redirect(`${ABS_BASE_PATH}/guard`);
+});
+
+router.post('/guard', function (req, res) {
+  res.redirect(`${NEXT_PATH}`);
+});
+
+module.exports = router;
