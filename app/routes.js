28a66bb2df89d7c984d3b09d1cde768385edf6e9
gazjoy <post@garethjoyce.co.uk>
2021-08-03 14:54:06 +0100

    [Sprint 8] journey to prepare users to verify online

--- app/routes.js
+++ app/routes.js
@@ -3,8 +3,19 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
-router.use('/sprint1/', require('./views/sprint1/_routes'))
-router.use('/sprint2/', require('./views/sprint2/_routes'))
+// Sprint 8 routes
+router.use('/sprint8/1/', require('./views/sprint8/1/_routes'))
+router.use('/sprint8/1/1-eligibility/', require('./views/sprint8/1/1-eligibility/_routes'))
+router.use('/sprint8/1/2-details/', require('./views/sprint8/1/2-details/_routes'))
+router.use('/sprint8/1/3-claim-start/', require('./views/sprint8/1/3-claim-start/_routes'))
+router.use('/sprint8/1/4-other-benefits/', require('./views/sprint8/1/4-other-benefits/_routes'))
+router.use('/sprint8/1/5-jury-service/', require('./views/sprint8/1/5-jury-service/_routes'))
+router.use('/sprint8/1/6-current-employment/', require('./views/sprint8/1/6-current-employment/_routes'))
+router.use('/sprint8/1/7-previous-employment/', require('./views/sprint8/1/7-previous-employment/_routes'))
+router.use('/sprint8/1/8-abroad/', require('./views/sprint8/1/8-abroad/_routes'))
+router.use('/sprint8/1/9-pensions/', require('./views/sprint8/1/9-pensions/_routes'))
+router.use('/sprint8/1/10-education/', require('./views/sprint8/1/10-education/_routes'))
+router.use('/sprint8/1/dth/', require('./views/sprint8/1/dth/_routes'))
 
 // Sprint 4 routes
 router.use('/sprint4/', require('./views/sprint4/_routes'))
@@ -37,5 +48,11 @@ router.use('/sprint4/hypothesis2/v2/1-eligibility/', require('./views/sprint4/hy
 router.use('/sprint4/hypothesis2/v2/dth/', require('./views/sprint4/hypothesis2/v2/dth/_routes'))
 router.use('/sprint4/hypothesis2/v2/3-details/', require('./views/sprint4/hypothesis2/v2/3-details/_routes'))
 router.use('/sprint4/hypothesis2/v2/4-claim-start/', require('./views/sprint4/hypothesis2/v2/4-claim-start/_routes'))
+
+// Sprint 2 routes
+router.use('/sprint2/', require('./views/sprint2/_routes'))
+
+// Sprint 1 routes
+router.use('/sprint1/', require('./views/sprint1/_routes'))
 
 module.exports = router
